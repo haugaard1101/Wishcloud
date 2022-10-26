f3d2b1bc86f04158571c01c22b108a9ca4fa9b45
haugaard1101 <s205434@student.dtu.dk>
2022-10-26 12:12:17 +0200

    intial commit

---
+++ src/main/java/com/example/wishcloud/WishcloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.wishcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishcloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishcloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/wishcloud/WishcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wishcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WishcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
