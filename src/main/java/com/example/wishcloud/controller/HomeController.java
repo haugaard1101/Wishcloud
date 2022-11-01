89980c435d90f79db894fca94e827c7637f79642
Mauf8 <marc32n@stud.kea.dk>
2022-11-01 11:56:50 +0100

    program færdigt

--- src/main/java/com/example/wishcloud/controller/HomeController.java
+++ src/main/java/com/example/wishcloud/controller/HomeController.java
@@ -18,7 +18,7 @@ public class HomeController {
 
     @GetMapping("/")
     public String homePage() {
-        return "HomePage";
+        return "createwishlist";
     }
 
     @PostMapping("/createwish")
